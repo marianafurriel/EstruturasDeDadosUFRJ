ea631ad89c01853d14f43d2acf09225e50245e9a
Mariana Furriel <marianafurrielss@gmail.com>
2022-06-15 16:34:12 -0300

    ordenação topologica

--- ord.c
+++ ord.c
@@ -2,37 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define TAM 100
-
-typedef struct _tno
-{
-    struct _tno *adjacencias[TAM];
-    int grau;
-}tno;
+#define TAM 1000
 
 int main(int argc, char const *argv[])
 {
-    char vertices[TAM];
-    char arestas[TAM];
+    int vertices, arestas, aux, pos = 0;
+    char c;
+    int auxNum[TAM];
     char linha[TAM];
-    char numero[TAM];
-    int vert = 0;
-    int ares = 0;
-    scanf("%s", vertices);
-    scanf("%s", arestas);
-    vert = atoi(vertices);
-    ares = atoi(arestas);
-    int adjacencias[vert][vert];
-    int *nos = malloc(sizeof(tno) * vert);
-    for (int i = 0; i < vert; i++)
-    {
-        for(int j =0;j<vert;j++){
-            
-        }
-        scanf("%s",numero);
-        while(numero!='\n'){
+    int ordem[TAM];
+    scanf("%d", &vertices);
+    scanf("%d", &arestas);
+    c = getchar();
+    int *adj = malloc(sizeof(int) * vertices * vertices);
 
+    for (int i = 0; i < vertices; i++) // preenchendo a matriz de adjacencias toda com 0
+    {
+        for (int j = 0; j < vertices; j++)
+        {
+            adj[(i * vertices) + j] = 0;
         }
     }
+
+    for (int i = 0; i < vertices; i++) // preenchendo a tabela de adjacencias
+    {
+        printf("adjacencias do nó %d\n", i + 1);
+        fgets(linha, TAM, stdin);
+        for (int j = 0; j < strlen(linha); j++)
+        {
+            while (isdigit(linha[j]))
+            {
+                auxNum[pos] = linha[j];
+                j++;
+                pos++;
+            }
+            auxNum[pos] = '\0';
+            pos = 0;
+            adj[(i * vertices) + atoi(auxNum) - 1] = 1;
+        }
+    }
+
+    for (int i = 0; i < vertices; i++)
+    {
+        for (int j = 0; j < vertices; j++)
+        {
+            printf("\t%d", adj[(i * vertices) + j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
